fc1c6e1975a4469fce7e3a0bc67f6dee472a7f5a
Henry <henry.a.s.arrovin@gmail.com>
2023-10-31 17:53:38 +0530

    Added CORS

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 from waitress import serve
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the trained model and vectorizer
 classifier = joblib.load("sentiment_analysis_model.pkl")
