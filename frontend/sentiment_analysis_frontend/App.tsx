d6748b77558368083e5debf5de7da7dfbb7e3fed
Henry <henry.a.s.arrovin@gmail.com>
2023-10-31 18:43:24 +0530

    Posted request using axios

--- frontend/sentiment_analysis_frontend/App.tsx
+++ frontend/sentiment_analysis_frontend/App.tsx
@@ -3,9 +3,27 @@ import AppBar from "./components/AppBar";
 import MyTextField from "./components/MyTextField";
 import { useState } from "react";
 import MyButton from "./components/MyButton";
+import axios from "axios";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [sentiment, setSentiment] = useState("");
+
+  const handleButtonClick = async () => {
+    try {
+      const response = await axios.post(
+        "http://10.0.2.2:5000/predict_sentiment",
+        {
+          text: text,
+        }
+      );
+
+      setSentiment(JSON.stringify(response.data.sentiment));
+      console.log(sentiment);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -15,7 +33,10 @@ export default function App() {
           hintText="Enter text"
           onChangeText={(text) => setText(text)}
         />
-        <MyButton title="Click" onPress={() => {}} />
+        <MyButton title="Click" onPress={handleButtonClick} />
+        <View style={styles.textContainer}>
+          <Text style={styles.textDesign}>{sentiment.replace(/"/g, "")}</Text>
+        </View>
       </View>
     </View>
   );
@@ -29,5 +50,13 @@ const styles = StyleSheet.create({
   center: {
     flex: 1,
     justifyContent: "center",
+  },
+  textContainer: {
+    // padding: 3,
+    alignItems: "center",
+  },
+  textDesign: {
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
