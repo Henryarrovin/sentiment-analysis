24dd192ec0e72f091823a1d7114c4268ec464c00
Henry <henry.a.s.arrovin@gmail.com>
2023-10-31 17:10:32 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import joblib
+from waitress import serve
+
+app = Flask(__name__)
+
+# Load the trained model and vectorizer
+classifier = joblib.load("sentiment_analysis_model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+
+@app.route("/predict_sentiment", methods=["POST"])
+def predict_sentiment():
+    try:
+        data = request.get_json()
+        text = data["text"]
+
+        # Preprocess the input text using the same vectorizer
+        input_vector = vectorizer.transform([text])
+
+        # Make a prediction
+        prediction = classifier.predict(input_vector)
+        sentiment = prediction[0]
+
+        response = {"sentiment": sentiment}
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+@app.route("/")
+def index():
+    return "Welcome to Sentiment Analysis API"
+
+
+if __name__ == "__main__":
+    print("Server is running ...")
+    serve(app, host="0.0.0.0", port=5000)
